f8ab2351e95f4c437edaa7cc50cefd984f292d16
Boogbro <willriddel@gmail.com>
2025-11-19 22:28:03 -0600

    Add id attribute to GrowthInfrastructure section

--- src/components/GrowthInfrastructure.tsx
+++ src/components/GrowthInfrastructure.tsx
@@ -75,7 +75,7 @@ const GrowthInfrastructure = () => {
   }, [visibleCards]);
 
   return (
-    <section ref={sectionRef} className="py-32 px-6 relative overflow-hidden">
+    <section ref={sectionRef} id="infrastructure" className="py-32 px-6 relative overflow-hidden">
       {/* Background glow */}
       <div className="absolute top-1/4 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-primary/5 rounded-full blur-[120px]"></div>
       
