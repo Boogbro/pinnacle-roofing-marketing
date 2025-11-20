6abe012ab9f25642c50d08c92602ce7e9f721b79
Boogbro <willriddel@gmail.com>
2025-11-19 22:24:32 -0600

    df

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,12 @@ import ROICalculator from "@/components/ROICalculator";
 import GrowthInfrastructure from "@/components/GrowthInfrastructure";
 import FAQ from "@/components/FAQ";
 import FinalCTA from "@/components/FinalCTA";
+import Navbar from "@/components/Navbar";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen pt-20"> {/* Added pt-20 to account for fixed navbar height */}
+      <Navbar />
       <Hero />
       <LogoCarousel />
       <SystemProcess />
